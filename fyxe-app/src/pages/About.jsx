b6ce991e7a2c54f58001bd7d4905d394733f02c5
hhk63 <hhk2@pdx.edu>
2025-03-05 00:46:18 -0800

    added About description

--- fyxe-app/src/pages/About.jsx
+++ fyxe-app/src/pages/About.jsx
@@ -11,7 +11,24 @@ export default function About() {
                     <h1>About</h1>
                 </div>
                 <div className="paragraph">
-                    <p>ADD INFO HERE</p>
+                    <p>
+                        As AI becomes more integrated into daily life, users, specifically those sharing sensitive
+                        information, must understand the potential privacy risks involved in interacting with
+                        mainstream AI bots. Our platform raises awareness about the collection and use of user data
+                        by these AI-powered tools.
+                    </p>
+                    <p>
+                        We aim to educate users about privacy issues and provide practical solutions. One key feature
+                        is a tool that sanitizes user prompts, ensuring that personal data is protected before being
+                        submitted to CAs. This tool acts as a safeguard, allowing users to interact with CAs while
+                        maintaining their privacy.
+                    </p>
+                    <p>
+                        By offering resources and insights, our website helps users make informed decisions,
+                        enhances privacy literacy, and advocates for greater transparency in the practices of
+                        conversational AI. Our goal is to empower users with the knowledge and tools needed
+                        to protect their privacy in an increasingly AI-driven world.
+                    </p>
                 </div>
             </div>
         </div>
